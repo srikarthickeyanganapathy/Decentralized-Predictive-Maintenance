0513c20577b188c0dc5caa6cc67293fada217cb4
srikarthickeyanganapathy <srikarthcikeyanganapathy@gmail.com>
2024-12-19 00:55:41 +0530

    Updated requirement file

--- DPM/flask/app.py
+++ DPM/flask/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 from flask_cors import CORS
+import os  # Import os to read environment variables
+
 app = Flask(__name__)
 CORS(app)
 
@@ -27,4 +29,5 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
