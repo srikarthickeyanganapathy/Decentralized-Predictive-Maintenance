7533779ba38b459e720808facfb044a0b511cdd1
srikarthickeyanganapathy <srikarthcikeyanganapathy@gmail.com>
2024-12-18 22:41:28 +0530

    Deleted code files

--- DPM/backend/routes/DPMroutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const {LogPrediction, getAllData} = require('../controllers/DPMcontrollers');
-const router = express.Router();
-
-router.post('/predictAndLog', LogPrediction);
-router.get('/getall', getAllData);
-module.exports = router;
